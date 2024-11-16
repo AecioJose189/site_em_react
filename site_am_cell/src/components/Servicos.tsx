47654675581c4dc86e8079d5d154486cd0eabb13
aeciojose <aeciojunior45@gmail.com>
2024-11-16 14:03:56 -0300

    ta_tudo_baguncado

---
+++ site_am_cell/src/components/Servicos.tsx
@@ -0,0 +1,11 @@
+import './Servicos.css'
+
+function Servicos() {
+    return(
+        <header className='servicos-title'>
+            <h1>Serviços</h1>
+        </header>
+    )
+}
+
+export default Servicos
--- site_am_cell/src/main.tsx
+++ site_am_cell/src/main.tsx
@@ -4,13 +4,13 @@ import './index.css'
 import Carrossel from './components/Carrossel'
 import NavBar from './components/NavBar'
 import SobreNos from './components/SobreNos'
-import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Servicos from './components/Servicos'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <NavBar />
     <Carrossel/>
     <SobreNos/>
+    <Servicos/>
   </StrictMode>
 )
