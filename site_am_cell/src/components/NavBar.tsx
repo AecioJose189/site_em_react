bd54d3e6b8a82fd77d66eba632986616e37e19f9
aeciojose <aeciojunior45@gmail.com>
2024-11-16 08:16:51 -0300

    algumas_alteracoes_iniciais

---
+++ site_am_cell/src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+
+import './Navbar.css'
+function NavBar() {
+    return(
+        <header className="header">
+            <a href="/" className="logo">Logo amcell</a>
+            <nav className="navbar">
+                <a href="/">Início</a>
+                <a href="/">Nossos serviços</a>
+                <a href="/">Sobre nós</a>
+                <a href="/">Contato</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
